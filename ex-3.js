0905518e85d26b18df93ff1dd072d100d6953ce1
Teaboyd <ph.patiphan@kkumail.com>
2024-03-26 08:49:18 +0700

    Complete primitive values by Teaboyd

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,6 @@
 
 let totalFileSize = 250000;
 let uploadedSize = 30000;
-
+let uploadProgress = `Progress up load is ${totalFileSize/uploadedSize} percent`;
+console.log(uploadProgress);
 // Start coding here
